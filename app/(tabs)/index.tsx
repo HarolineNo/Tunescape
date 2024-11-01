54f3bdded9594048b5ae5d0fd08b784d302430c2
harolinenoesi <harolinenoesi@gmail.com>
2024-11-01 17:36:28 -0400

    added pages for quis instructions and quiz

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,10 +8,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import TabFiveScreen from './signup';
 import TabSixScreen from './login';
+import Instructions from './quizInstructions';
+import Quiz from './quiz';
 
 export type RootStackParamList = {
   Signup: undefined;
   Login: undefined;
+  Instructions: undefined;
+  Quiz: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,6 +25,8 @@ export default function HomeScreen() {
     <Stack.Navigator>
       <Stack.Screen name='Signup' component={TabFiveScreen} />
       <Stack.Screen name='Login' component={TabSixScreen} />
+      <Stack.Screen name='Instructions' component={Instructions} />
+      <Stack.Screen name='Quiz' component={Quiz} />
     </Stack.Navigator>  
   );
 }
---
+++ app/(tabs)/quiz.tsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, ImageBackground, View, ScrollView, Text, TouchableOpacity, TextInput, Button } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as SecureStore from 'expo-secure-store';
+import Checkbox from 'expo-checkbox';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from './index';
+
+type QuizScreen = NativeStackNavigationProp<RootStackParamList, 'Quiz'>;
+
+export default function Quiz() {
+    return (
+    <SafeAreaView style={styles.container}>
+        <TouchableOpacity style={styles.signinBtn}>
+            <Text>Next</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#08081e',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    background: {
+        flex: 1,
+    },
+    signinContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2e2cb2',
+        height: '60%',
+        width: '85%',
+        borderRadius: 20
+    },
+    titleText: {
+        color: 'white',
+        fontSize: 40,
+        textAlign: 'center',
+    },
+    chooseBtns: {
+        flexDirection: 'row',
+        width: '85%',
+        height: '11%',
+        justifyContent: 'center',
+        marginBottom: 50,
+        marginTop: 20,
+    },
+    activeBtn: {
+        borderRadius: 10,
+        width: '40%',
+        backgroundColor: '#c5d6ff',
+        marginLeft: 30,
+    },
+    btn: {
+        borderRadius: 10,
+        width: '40%',
+        backgroundColor: 'blue',
+    },
+    btnText: {
+        flex: 1,
+        fontSize: 24,
+        padding: 10,
+        color: 'white',
+        textAlign: 'center',
+    },
+    inputField: {
+        backgroundColor: '#7797e8',
+        height: '10%',
+        width: '77%',
+        borderRadius: 5,
+        marginBottom: 20,
+        padding: 10
+    },
+    passwordField: {
+        flex: 1,
+    },
+    passwordContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#7797e8',
+        borderRadius: 5,
+        width: '77%',
+        height: '10%',
+        marginBottom: 5,
+        padding: 10
+    },
+    signinBtn: {
+        borderRadius: 10,
+        width: '77%',
+        height: '11%',
+        backgroundColor: 'blue',
+        marginTop: 10
+    },
+    remeberMeContainer: {
+        width: '77%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 10
+    },
+    checkbox: {
+        borderColor: 'white',
+        borderWidth: 1,
+        width: 12,
+        height: 12,
+        marginRight: 5
+    },
+});
